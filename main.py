949d65ab43b5098a6cd7d14ec2459ac9ba87b1eb
Alex Ainea <xo.ainea@gmail.com>
2026-01-08 14:31:49 +0300

    auth form centering

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.geometry("1500x1500")
-root.title("TASK MANAGER")
+auth = tk.Tk()
+auth.geometry("500x500")
+auth.title("TASK MANAGER")
+auth.config(bg = "white")
 
-root.mainloop()
+window_width = 600
+window_height = 400
+
+screen_width = auth.winfo_screenwidth()
+screen_height = auth.winfo_screenheight()
+
+x = (screen_width // 2) - (window_width // 2)
+y = (screen_height // 2) - (window_height // 2)
+
+auth.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+auth.mainloop()
