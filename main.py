ea8ecaa1ccab47753e8b7998604b5a3a45dd2a8a
Alex Ainea <xo.ainea@gmail.com>
2026-01-17 21:12:55 +0300

    log in and sign up button placement

--- main.py
+++ main.py
@@ -1,37 +1,50 @@
 import tkinter as tk
 
-auth = tk.Tk()
-auth.geometry("500x500")
-auth.title("TASK MANAGER")
-auth.config(bg = "#87CEEB")
+current_frame_auth = None
+def authentication():
+    global current_frame_auth
 
-window_width = 600
-window_height = 400
+    auth = tk.Tk()
+    auth.geometry("500x500")
+    auth.title("TASK MANAGER")
+    auth.config(bg = "#87CEEB")
 
-screen_width = auth.winfo_screenwidth()
-screen_height = auth.winfo_screenheight()
+    window_width = 600
+    window_height = 400
 
-x = (screen_width // 2) - (window_width // 2)
-y = (screen_height // 2) - (window_height // 2)
+    screen_width = auth.winfo_screenwidth()
+    screen_height = auth.winfo_screenheight()
 
-auth.geometry(f"{window_width}x{window_height}+{x}+{y}")
+    x = (screen_width // 2) - (window_width // 2)
+    y = (screen_height // 2) - (window_height // 2)
 
-log_in_frame = tk.Frame(auth, bg="#87CEEB")
-log_in_frame.pack()
+    auth.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-username_log_in_label = tk.Label(log_in_frame, text="USERNAME: ", bg="#87CEEB", fg="black")
-username_log_in_label.grid(row = 0, column=0)
+    log_in_button_auth_form = tk.Button(auth,text="LOG IN",bg="white",fg="black",activebackground="white",activeforeground="black",highlightbackground="#87CEEB",highlightthickness="0",padx=10, pady=4, cursor="hand2")
+    log_in_button_auth_form.grid(padx= 150,pady=150, row=0, column=0)
 
-username_log_in_entry = tk.Entry(log_in_frame, bg="#87CEEB", fg="black", bd=0, highlightthickness = 1)
-username_log_in_entry.grid(row = 0, column=1)
+    sign_up_button_auth_form = tk.Button(auth,text="SIGN UP",bg="white",fg="black",activebackground="white",activeforeground="black",highlightbackground="#87CEEB",highlightthickness="0",padx=10, pady=4, cursor="hand2")
+    sign_up_button_auth_form.grid(row=0, column=1)
 
-password_log_in_label = tk.Label(log_in_frame, text="PASSWORD: ", bg="#87CEEB", fg="black")
-password_log_in_label.grid(row = 1, column=0)
+    def log_in_function():
+        log_in_frame = tk.Frame(auth, bg="#87CEEB")
+        log_in_frame.pack()
 
-password_log_in_entry = tk.Entry(log_in_frame, bg="#87CEEB", fg="black", bd=0, highlightthickness = 1, show = '*')
-password_log_in_entry.grid(row = 1, column=1)
+        username_log_in_label = tk.Label(log_in_frame, text="USERNAME: ", bg="#87CEEB", fg="black")
+        username_log_in_label.grid(row = 0, column=0)
 
-log_in_button = tk.Button(log_in_frame, text="LOG IN", bg="white",fg="black", activebackground="white", activeforeground="black",borderwidth=0, highlightthickness=0,highlightbackground="white", relief="flat", padx=10, pady=4, cursor="hand2")
-log_in_button.grid(row = 2, column= 0)
+        username_log_in_entry = tk.Entry(log_in_frame, bg="#87CEEB", fg="black", bd=0, highlightthickness = 1, relief="flat", borderwidth=0)
+        username_log_in_entry.grid(row = 0, column=1)
 
-auth.mainloop()
+        password_log_in_label = tk.Label(log_in_frame, text="PASSWORD: ", bg="#87CEEB", fg="black")
+        password_log_in_label.grid(row = 1, column=0)
+
+        password_log_in_entry = tk.Entry(log_in_frame, bg="#87CEEB", fg="black", bd=0, highlightthickness = 1, show = '*')
+        password_log_in_entry.grid(row = 1, column=1)
+
+        log_in_button = tk.Button(log_in_frame, text="LOG IN", bg="white",fg="black", activebackground="white", activeforeground="black",borderwidth=0, highlightthickness=0,highlightbackground="white", relief="flat", padx=10, pady=4, cursor="hand2")
+        log_in_button.grid(row = 2, column= 0)
+
+    auth.mainloop()
+
+authentication()
